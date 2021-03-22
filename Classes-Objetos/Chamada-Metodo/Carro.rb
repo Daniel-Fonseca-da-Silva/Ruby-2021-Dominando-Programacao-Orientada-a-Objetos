61ade8f5aff088793e8a981b315673f1c48908cc
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2021-03-22 20:07:13 -0300

    Adicionado arquivo Carro.rb na pasta Chamada-Metodo

---
+++ Classes-Objetos/Chamada-Metodo/Carro.rb
@@ -0,0 +1,11 @@
+
+class Carro # Classe template
+    def ligar_motor # Nome do método
+        puts("Vrum vrum!") # Comando que exibe um texto
+    end # Final do método
+end # Final da clasase
+
+fusca = Carro.new # Criação do objeto fusca usando a classe Carro
+
+fusca.ligar_motor # Chamando o método do objeto
+# Fim
