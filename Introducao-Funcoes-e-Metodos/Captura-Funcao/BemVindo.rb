afbec6506368596af2e9072caca50039506487c6
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2021-03-22 20:03:40 -0300

    Adicionado arquivo BemVindo.rb na pasta Captura-Funcao

---
+++ Introducao-Funcoes-e-Metodos/Captura-Funcao/BemVindo.rb
@@ -0,0 +1,11 @@
+puts('Entre com o seu nome:' ) # Exibe um texto para o usuário
+nome = gets # Define uma variável e guarda o valor informado nela
+puts("Bem-vindo #{nome}") # Exibe um texto junto de uma variável
+
+# Diferença de print e puts
+
+=begin
+    Print = comando que não quebrar a linha 
+    puts = comando que irá quebra a linha
+=end
+
